03d6608d1998e2f0edd9485e439733ec4bd0a60a
notorangee <yundaoju@gmail.com>
2026-08-11 16:38:59 +0800

    Modify Configuration

--- config.def.h
+++ config.def.h
@@ -13,7 +13,7 @@ static const float bordercolor[]           = COLOR(0x2d2c2cff);
 static const float focuscolor[]            = COLOR(0x9b8beeff);
 static const float urgentcolor[]           = COLOR(0xff0000ff);
 /* This conforms to the xdg-protocol. Set the alpha to zero to restore the old behavior */
-static const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 1.0f}; /* You can also use glsl colors */
+static const float fullscreen_bg[]         = {0.0f, 0.0f, 0.0f, 0.0f}; /* You can also use glsl colors */
 
 /* tagging - TAGCOUNT must be no greater than 31 */
 #define TAGCOUNT (6)
